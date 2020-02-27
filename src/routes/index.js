0e00fb64c97d1129defaf022457c01c5faafddc9
danigb66 <danigb66@gmail.com>
2020-02-26 22:08:56 -0500

    dsd

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,8 +2,10 @@ const express = require('express');
 const router = express.Router();
 const { isNotLoggedIn } = require('../lib/auth');
 
-router.get('/',isNotLoggedIn, (req, res) => {
+/* router.get('/',isNotLoggedIn, (req, res) => {
     res.render('auth/signin');
+}); */
+router.get('/', (req, res) => {
+    res.send('Test');
 });
-
 module.exports = router;
